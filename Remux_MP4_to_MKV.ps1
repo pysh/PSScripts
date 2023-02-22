85b312f77bf328f5d1ca4615c613d8b3bb7373a8
pysh <paul.nosov@gmail.com>
2023-02-22 10:22:59 +0300

    refactor

--- Remux_MP4_to_MKV.ps1
+++ Remux_MP4_to_MKV.ps1
@@ -3,8 +3,8 @@
 Clear-Host
 $ffmpeg = 'C:\Users\pauln\AppData\Roaming\FFmpeg\bin\ffmpeg.exe'
 $InputPath = 'w:\.temp\Youtube\'
-$InputFilesMask = @("*.webm","*.mp4")
-$OutputPath= '' #'w:\.temp\Youtube\Разгоны_out'
+$InputFilesMask = @("*.webm", "*.mp4")
+$OutputPath = '' #'w:\.temp\Youtube\Разгоны_out'
 Write-Host "Scanning the directory for files..."
 $InputFileList = (Get-ChildItem -LiteralPath $InputPath -File -Include $InputFilesMask -Recurse)
 Write-Host ("Found files: {0}" -f $InputFileList.Count) -ForegroundColor Yellow
@@ -14,25 +14,27 @@ foreach ($InputMP4 in $InputFileList) {
     if ($OutputPath -eq '') {
         #$OutputMKV      = ("{0}\{1}.{2}" -f $InputMP4.DirectoryName, $InputMP4.BaseName, "mkv")
         #$OutputMKV      = ("{0}\{1}.{2}" -f $InputMP4.DirectoryName, $InputMP4.Directory.Name, "mkv")
-        $OutputMKV = ("{0}\{1}_[{3}].{2}" -f $InputMP4.DirectoryName, $InputMP4.Directory.Name, "mkv", $InputMP4.Extension.Remove(0,1))
-    } else {
+        $OutputMKV = ("{0}\{1}_[{3}].{2}" -f $InputMP4.DirectoryName, $InputMP4.Directory.Name, "mkv", $InputMP4.Extension.Remove(0, 1))
+    }
+    else {
         #$OutputMKV = ("{0}\{1}.{2}" -f $OutputPath, $InputMP4.BaseName, "mkv")
-        $OutputMKV = ("{0}\{1}_[{3}].{2}" -f $OutputPath, $InputMP4.Directory.Name, "mkv", $InputMP4.Extension.Remove(0,1))
+        $OutputMKV = ("{0}\{1}_[{3}].{2}" -f $OutputPath, $InputMP4.Directory.Name, "mkv", $InputMP4.Extension.Remove(0, 1))
     }
     
     if (Test-Path -LiteralPath $OutputMKV -PathType Leaf) {
         Write-Host ("File {0} exists. Skipping." -f $OutputMKV) -ForegroundColor Magenta
-    } else {
+    }
+    else {
         Write-Host ("{0} / {1}" -f $n, $InputFileList.Count) -ForegroundColor Cyan
         #$InputCover = ("{0}\{1}" -f $InputMP4.DirectoryName, "cover.jpg")
         $InputCover = Get-ChildItem -LiteralPath $InputMP4.DirectoryName -File -Filter '*.jpg'
         $InputDescr = Get-ChildItem -LiteralPath $InputMP4.DirectoryName -File -Filter '*description*.txt'
-        $ArgList =  ("-hide_banner", "-i ""$InputMP4""", 
-                        '-map 0:0 -map 0:1 -map_metadata -1:s:0 -map_metadata 0:g:0 -c copy -metadata:s:a:0 language=ru -metadata:s:v:0 language=rus', 
-                        "-attach ""$InputCover"" -metadata:s:t:0 mimetype=image/jpeg -metadata:s:t:0 filename=cover.jpg", 
-                        "-attach ""$InputDescr"" -metadata:s:t:1 mimetype=text/plain -metadata:s:t:1 filename=description.txt", 
-                        "-y", 
-                        """$OutputMKV""")
+        $ArgList = ("-hide_banner", "-i ""$InputMP4""", 
+            '-map 0:0 -map 0:1 -map_metadata -1:s:0 -map_metadata 0:g:0 -c copy -metadata:s:a:0 language=ru -metadata:s:v:0 language=rus', 
+            "-attach ""$InputCover"" -metadata:s:t:0 mimetype=image/jpeg -metadata:s:t:0 filename=cover.jpg", 
+            "-attach ""$InputDescr"" -metadata:s:t:1 mimetype=text/plain -metadata:s:t:1 filename=description.txt", 
+            "-y", 
+            """$OutputMKV""")
         #Write-Host "Remuxing file: " -NoNewLine
         Write-Host ("{0}\" -f $OutputPath) -NoNewLine -ForegroundColor Blue
         Write-Host $InputMP4.Name -ForegroundColor DarkBlue
